aa284e0c3618c6af65b84c3bf2d1ea9dc6ea6ce7
itzaddddd <isarafff@gmail.com>
2020-05-13 10:51:42 +0700

    merge profile and login

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import MyDiaryScreen from './screen/diary/MyDiaryScreen'
 import EatChallengeScreen from './screen/eating/CreateEatingChellengeScreen'
 import WorkoutChallengeScreen from './screen/workout/CreateWorkoutChellengeScreen'
 import FavDiaryScreen from './screen/diary/FavDiaryScreen'
-import LoginScreen from './screen/auth/LoginScreen'
+//import LoginScreen from './screen/auth/LoginScreen'
 //import SignUpScreen from './screen/auth/Signup.Screen'
 import ProfileScreen from './screen/auth/ProfileScreen'
 //import EditProfileScreen from './screen/auth/EditProfileScreen'
@@ -48,7 +48,6 @@ export default function App() {
           <Drawer.Screen name="EatChallenge" component={EatChallengeScreen} options={{title:"ชาเลนจ์การกิน"}} />
           <Drawer.Screen name="WorkoutChallenge" component={WorkoutChallengeScreen} options={{title:"ชาเลนจ์ออกกำลังกาย"}} />
           <Drawer.Screen name="FavDiary" component={FavDiaryScreen} options={{title:"ไดอารี่โปรด"}} />
-          <Drawer.Screen name="Login" component={LoginScreen} options={{title:"เข้าสู่ระบบ"}} />
           <Drawer.Screen name="Profile" component={ProfileScreen} options={{title:"โปรไฟล์"}} />
           <Drawer.Screen name="WorkoutDetail" component={WorkoutDetailScreen} options={{title:"รายละเอียดชาเลนจ์ออกกำลังกาย"}} />
           
